9e5bae29b9cb7f2695c753e3153d58b3a046de89
Markus Feng <zzz1112222595@hotmail.com>
2014-09-28 14:03:08 -0400

    Added libgdx setup

---
+++ desktop/src/com/github/pixelrunstudios/GdxTest/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.github.pixelrunstudios.GdxTest.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.github.pixelrunstudios.GdxTest.GdxTest;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GdxTest(), config);
+	}
+}
