4aad98eeb23c63b620888887078186ed592d7a48
Markus Feng <zzz1112222595@hotmail.com>
2014-09-29 15:30:30 -0400

    Added fonts
    Wrote Hello, world!

--- core/src/com/github/pixelrunstudios/GdxTest/GdxTest.java
+++ core/src/com/github/pixelrunstudios/GdxTest/GdxTest.java
@@ -2,12 +2,15 @@ package com.github.pixelrunstudios.GdxTest;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class GdxTest extends ApplicationAdapter {
 
+	protected BitmapFont font;
 	protected SpriteBatch batch;
 	private Texture img;
 	protected PlatformIndependent platform;
@@ -23,7 +26,8 @@ public class GdxTest extends ApplicationAdapter {
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("cat.jpg");
+		img = new Texture(Gdx.files.internal("cat.jpg"));
+		font = new BitmapFont(Gdx.files.internal("font/helvetica-neue-20.fnt"));
 	}
 
 	@Override
@@ -32,6 +36,8 @@ public class GdxTest extends ApplicationAdapter {
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		batch.draw(img, 0, 0);
+		font.setColor(Color.BLACK);
+		font.draw(batch, "Hello, world!", 100, 100);
 		batch.end();
 	}
 }
