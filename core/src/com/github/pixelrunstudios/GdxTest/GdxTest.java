60c1f1fea7fdf1019f7571a524d27d30a2b33965
Markus Feng <zzz1112222595@hotmail.com>
2014-09-29 14:23:35 -0400

    Minor changes
    Changed background color to blue

--- core/src/com/github/pixelrunstudios/GdxTest/GdxTest.java
+++ core/src/com/github/pixelrunstudios/GdxTest/GdxTest.java
@@ -7,9 +7,14 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class GdxTest extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	PlatformIndependent platform;
+
+	protected SpriteBatch batch;
+	private Texture img;
+	protected PlatformIndependent platform;
+
+	protected GdxTest(){
+
+	}
 
 	public GdxTest(PlatformIndependent platform){
 		this.platform = platform;
@@ -23,7 +28,7 @@ public class GdxTest extends ApplicationAdapter {
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0, 0, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		batch.draw(img, 0, 0);
